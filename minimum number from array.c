7311301c1986b88153fe7b1d275801baf28c7533
SHAIK NOORUDDIN <144267152+klu2200040145@users.noreply.github.com>
2024-08-26 16:11:44 +0530

    Create minimum number from array.c

---
+++ minimum number from array.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void fun(int n){
+    int a[n];
+    printf("Enter Elements: ");
+    for(int i = 0; i < n; i++){
+        scanf("%d", &a[i]);
+    }
+    printf("Elements are : ");
+    for(int i = 0; i < n; i++){
+        printf("%d ", a[i]);
+    }
+    printf("\nminimum number : ");
+    int min = a[0];
+    for(int i = 1; i < n; i++){
+        if(min > a[i])
+        min = a[i];
+    }
+    printf("%d\t", min);
+}
+void main(){
+    int n, r;
+    printf("Enter size : ");
+    scanf("%d", &n);
+    fun(n);
+}
