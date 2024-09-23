46af71fc6de15e2de0fb19320d06dd147a105106
SHAIK NOORUDDIN <144267152+klu2200040145@users.noreply.github.com>
2024-09-23 11:42:15 +0530

    Create pattern9.c

---
+++ pattern9.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void fun(int a){
+    for(int i = a - 1; i >= 1; i--){
+        for(int k = 1; k <= 32 - i*2; k++){
+            printf(" ");
+        }
+        for(int j = 1; j <= i * 2 - 1; j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
+void main(){
+    int a;
+    printf("Enter the stair size : ");
+    scanf("%d", &a);
+    fun(a);
+}
