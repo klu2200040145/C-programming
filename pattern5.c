7132b2113c9472bcc8107ea9d4647ea018ee0bd5
SHAIK NOORUDDIN <144267152+klu2200040145@users.noreply.github.com>
2024-09-23 10:52:36 +0530

    pattern5.c

---
+++ pattern5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void fun(int a){
+    for(int i = 1; i <= a; i++){
+        for(int j = 1; j <= i; j++){
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+}
+void main(){
+    int a;
+    printf("Enter the stair size : ");
+    scanf("%d", &a);
+    fun(a);
+}
